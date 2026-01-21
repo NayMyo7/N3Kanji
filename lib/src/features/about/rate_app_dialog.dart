165b13185651cbcbed9a1ec59c8ccf66e9f2078b
Nay Myo Htet <naymyohtet.hmn@gmail.com>
2026-01-22 06:12:24 +0630

    Update iOS app store URL and increment build number

--- lib/src/features/about/rate_app_dialog.dart
+++ lib/src/features/about/rate_app_dialog.dart
@@ -8,7 +8,7 @@ class RateAppDialog extends StatelessWidget {
 
   static const String _androidUrl =
       'https://play.google.com/store/apps/details?id=com.dragondev.n3kanji';
-  static const String _iosUrl = 'https://apps.apple.com/app/id6757802869';
+  static const String _iosUrl = 'https://apps.apple.com/app/id6758101216';
 
   @override
   Widget build(BuildContext context) {
